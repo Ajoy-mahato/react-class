0ea1065b007e6c3adc1a84c7ac9876005e19a552
Ajoy Mahato <mahatoajoy389@gmail.com>
2025-11-03 13:49:45 +0530

    part-2 done

--- 01_folder/src/App.jsx
+++ 01_folder/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import  "./App.css"
 const App = () => {
+  const user="ajoy";
   return (
     <div>
-      hello world
-      
+      <div className='card'>
+         <h1>welcom</h1>
+         <p>{user}</p>
+      </div>
+      <div className='card'>
+         <h1>Ajoy mahato</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa suscipit tempore ea , itaque labore.</p>
+      </div>
+      <div className='card'>
+         <h1>Ajoy mahato</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa suscipit tempore ea , itaque labore.</p>
+      </div>
+      <div className='card'>
+         <h1>Ajoy mahato</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa suscipit tempore ea , itaque labore.</p>
+      </div>
+
     </div>
   )
 }
